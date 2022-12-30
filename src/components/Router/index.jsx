c79c8783897cebd2068dac0889aa0e8b5db9bce7
A_Larose <larose.alain@gmail.com>
2022-12-30 01:36:55 +0100

    Ajout de React Router

---
+++ src/components/Router/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default Router;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import Router from './components/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  </React.StrictMode>
+    <RouterProvider router={Router} />
+  </React.StrictMode>,
 );
-
